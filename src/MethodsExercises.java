96d32fb3b244400db0ff764b67fb772ed2960fc5
steven hampton <steven.hampton415@gmail.com>
2018-05-02 11:25:31 -0500

    Finished main problems from MethodsExercises

---
+++ src/MethodsExercises.java
@@ -0,0 +1,20 @@
+public class MethodsExercises {
+    public static void main(String[] args) {
+        System.out.println(addition(2, 4));
+        System.out.println(subtraction(5,3));
+        System.out.println(multiplication(10,5));
+        System.out.println(division(15,5));
+    }
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int multiplication(int num1, int num2){
+        return num1 * num2;
+    }
+    public static int division(int num1, int num2){
+        return num1 / num2;
+    }
+}
