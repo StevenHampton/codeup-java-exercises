06aeb8e86aeea081cc596766babbeaab2f5b5cba
steven hampton <steven.hampton415@gmail.com>
2018-05-10 11:53:35 -0500

    abstract and interface setup

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,31 +1,6 @@
 package Shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-    public Rectangle(){}
 
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
-    }
-    public int getArea(){
-        return length * width;
-    }
-    public int getPerimeter(){
-        return (length *2) + (width * 2);
-    }
-    public void setWidth() {
-        this.width = width;
-    }
-    public int getWidth() {
-        return width;
-    }
-    public void setLength() {
-        this.length = length;
-    }
-    public int getLength() {
-        return length;
-    }
 }
 
