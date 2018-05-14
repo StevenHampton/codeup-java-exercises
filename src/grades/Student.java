6aade44395728d1103f5b952808026ac2bc9eb45
steven hampton <steven.hampton415@gmail.com>
2018-05-14 09:28:09 -0500

    set up basics in student

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,31 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String studentName;
+    private ArrayList<Integer> grades = new ArrayList<>();
+
+    public Student(String studentName, ArrayList<Integer> grades) {
+        this.studentName = studentName;
+        this.grades = grades;
+    }
+
+    public void addStudent(){
+
+    }
+
+    public void addGrade(){
+
+    }
+
+
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
 }
